ebfccc356cfba4560ee1dad54912f08b8dc7aaa5
Cai <93735823+CoderCailean@users.noreply.github.com>
2022-12-02 16:14:01 -0600

    Update admin.php
    Added the ability for an admin user to enable/disable other users, working on ability to allow admin to update user details such as email and admin access.

--- admin.php
+++ admin.php
@@ -9,11 +9,70 @@ require('connect.php');
 
 session_start();
 
-$users_query = "SELECT u.user_email, u.user_register, u.user_id, count(p.user_id) FROM users u
-JOIN post p ON u.user_id = p.user_id
+$updating = false;
+
+$users_query = "SELECT u.user_email, u.user_register, u.user_id, u.user_admin, u.active, count(p.user_id) FROM users u
+LEFT JOIN post p ON u.user_id = p.user_id
 GROUP BY p.user_id";
 $users_statement = $db->prepare($users_query);
 $users_statement->execute();
+
+
+if(isset($_POST['disable_user']))
+{
+    $user = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+    $disable = "UPDATE users SET active = 0 WHERE user_id = :id";
+    $disable_statement = $db->prepare($disable);
+    $disable_statement->bindValue(':id', $user);
+    $disable_statement->execute();
+
+    header('Location: admin.php');
+}
+
+if(isset($_POST['enable_user']))
+{
+    $user = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+
+    $user = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+    $enable = "UPDATE users SET active = 1 WHERE user_id = :id";
+    $enable_statement = $db->prepare($enable);
+    $enable_statement->bindValue(':id', $user);
+    $enable_statement->execute();
+
+    header('Location: admin.php');
+}
+
+if(isset($_POST['update_user']))
+{
+    $updating = true;
+}
+
+if(isset($_POST['submit_update']))
+{
+    if(filter_input(INPUT_POST, 'user_email_update', FILTER_VALIDATE_EMAIL))
+    {
+        $new_email = filter_input(INPUT_POST, 'user_email_update', FILTER_SANITIZE_SPECIAL_CHARS);
+        $user_access = filter_input(INPUT_POST, 'access_level', FILTER_SANITIZE_SPECIAL_CHARS);
+        $user_id = filter_input(INPUT_POST, 'user_being_updated', FILTER_SANITIZE_NUMBER_INT);
+
+        $update_query = "UPDATE users SET user_email = :email, user_admin = :access WHERE user_id = :id";
+        $update_statement = $db->prepare($update_query);
+        $update_statement->bindValue(':email', $new_email);
+        $update_statement->bindValue(':access', $user_access);
+        $update_statement->bindValue(':id', $user_id);
+        $update_statement->execute();
+
+        header('Location: admin.php');
+    }
+    
+}
+
+
+if(isset($_SESSION['current_user']))
+{
+    $index_of_at = strpos($_SESSION['current_user'], '@');
+    $username = substr($_SESSION['current_user'], 0, $index_of_at);
+}
 
 ?>
 <!DOCTYPE html>
@@ -38,7 +97,7 @@ $users_statement->execute();
         <li><a href="login.php">Login</a></li>
         <?php else: ?>
         <?php if ($_SESSION['current_user_admin'] == 1): ?>
-        <li><a href="admin.php">Admin Tools</a></li>
+        <li><a href="admin.php">Admin Center</a></li>
         <?php endif ?>
         <li><a href="logout.php">Logout</a></li>
         <?php endif ?>
@@ -46,18 +105,38 @@ $users_statement->execute();
 </header>
 <body>
     <?php if(isset($_SESSION['current_user'])): ?>
-    <p id="user">Welcome, <?= $_SESSION['current_user'] ?></p>
+    <p id="user">Welcome, <?= $username ?></p>
     <?php endif ?>
     <?php if ($_SESSION['current_user_admin'] == 1): ?>
     <div id="members">
+        <?php if($updating): ?>
+            <form method="post" action="admin.php">
+                <label>User Email:</label>
+                <input type="email" name="user_email_update" value="<?= $_POST['user_email'] ?>">
+                <label>Access Level (1 for Admin, 0 for User):</label>
+                <input type="text" name="access_level" value="<?= $_POST['user_admin'] ?>">
+                <input type="hidden" name="user_being_updated" value="<?= $_POST['user_id'] ?>" />
+                <button type="submit" name="submit_update">Update User</button>
+            </form>
+        <?php endif ?>
         <?php for($i = 0; $i < $users_statement->rowCount(); $i++): ?>
             <?php $row = $users_statement->fetch() ?>
             <div class="member">
                 <ul id="<?= $row['user_id'] ?>">
                     <li>Username: <?= $row['user_email'] ?></li>
                     <li>Member Since: <?= $row['user_register'] ?></li>
+                    <li>Admin: <?= $row['user_admin'] == 1 ? "Yes" : "No" ?></li>
                     <li>Number of Posts: <?= $row['count(p.user_id)'] ?></li>
+                    <li>Active: <?= $row['active'] == 1 ? "Yes" : "No" ?></li>
                 </ul>
+                <form method="post" action="admin.php">
+                    <input type="hidden" name="user_id" value="<?= $row['user_id'] ?>" />
+                    <input type="hidden" name="user_email" value="<?= $row['user_email'] ?>" />
+                    <input type="hidden" name="user_admin" value="<?= $row['user_admin'] ?>" />
+                    <button type="submit" name="disable_user">Disable User</button>
+                    <button type="submit" name="enable_user">Enable User</button>
+                    <button type="submit" name="update_user">Update User</button>
+                </form>
             </div>
         <?php endfor ?>
     </div>
