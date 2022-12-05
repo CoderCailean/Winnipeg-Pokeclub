a1c1b8c2c6705d89c0e4018210d693fb6047b2d9
Cai <93735823+CoderCailean@users.noreply.github.com>
2022-12-05 11:21:01 -0600

    Update edit.php
    Altered the file pathing for image uploads. New edit form allows users to delete their own post images and admins to delete images from any post.

--- edit.php
+++ edit.php
@@ -12,13 +12,31 @@ session_start();
 $errors = false;
 
 $requested_post = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$query = "SELECT * FROM post WHERE blog_id = :id";
+$query = "SELECT p.blog_id, p.user_id, p.post_title, p.post_desc, p.post_date, p.image_file, p.active, u.user_email
+          FROM post p JOIN users u ON p.user_id = u.user_id WHERE blog_id = :id";
 $statement = $db->prepare($query);
 $statement->bindValue(':id', $requested_post);
 $statement->execute();
+$row = $statement->fetch();
+
+
 
 if(isset($_POST['update']))
 {
+    if(isset($_POST['image_remove']))
+    {
+        $image_name = $_POST['image_remove'];
+        $author = $_POST['author'];
+        unlink("images/{$author}/{$image_name}");
+
+        $blog_id = filter_input(INPUT_POST, 'blog_id', FILTER_SANITIZE_NUMBER_INT);
+
+        $remove_image_query = "UPDATE post SET image_file = NULL WHERE blog_id = :id";
+        $remove_statement = $db->prepare($remove_image_query);
+        $remove_statement->bindValue(':id', $blog_id);
+        $remove_statement->execute();
+    }
+
     if(strlen($_POST['title']) > 1 && strlen($_POST['content']) > 1)
     {
         $new_title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
@@ -55,6 +73,11 @@ else if(isset($_POST['delete']))
     header('Location: index.php');
 }
 
+if(isset($_SESSION['current_user']))
+{
+    $index_of_at = strpos($_SESSION['current_user'], '@');
+    $username = substr($_SESSION['current_user'], 0, $index_of_at);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -78,7 +101,7 @@ else if(isset($_POST['delete']))
         <li><a href="login.php">Login</a></li>
         <?php else: ?>
         <?php if ($_SESSION['current_user_admin'] == 1): ?>
-        <li><a href="admin.php">Admin Tools</a></li>
+        <li><a href="admin.php">Admin Center</a></li>
         <?php endif ?>
         <li><a href="logout.php">Logout</a></li>
         <?php endif ?>
@@ -86,7 +109,7 @@ else if(isset($_POST['delete']))
 </header>
 <body>
     <?php if(isset($_SESSION['current_user'])): ?>
-    <p id="user">Welcome, <?= $_SESSION['current_user'] ?></p>
+    <p id="user">Welcome, <?= $username ?></p>
     <?php endif ?>
     <form method="get" action="search.php">
         <label>Search our Blogs:</label>
@@ -96,7 +119,6 @@ else if(isset($_POST['delete']))
     <?php if(isset($_SESSION['current_user'])): ?>
     <form method="post" action="edit.php">
         <?php if($statement->rowCount() > 0): ?>
-            <?php $row = $statement->fetch() ?>
             <?php if($_SESSION['current_user_id'] == $row['user_id'] || $_SESSION['current_user_admin'] == 1): ?>
             <label for="title">Post Title:</label>
             <input type="text" name="title" value="<?= $row['post_title'] ?>">
@@ -109,6 +131,11 @@ else if(isset($_POST['delete']))
             <?php if($errors): ?>
                 <p>*Must contain 1 or more characters.</p>
             <?php endif ?>
+            <?php if($row['image_file'] != NULL && $row['image_file'] != ""): ?>
+            <p>Remove image <?= $row['image_file'] ?> ?</p>
+            <input type="checkbox" name="image_remove" value="<?= $row['image_file'] ?>">
+            <?php endif ?>
+            <input type="hidden" name="author" value="<?= $row['user_email'] ?>" />
             <input type="hidden" name="blog_id" value="<?= $row['blog_id'] ?>" />
             <input type="submit" name="update" value="Update Post" />
             <input type="submit" name="delete" value="Delete Post" />
